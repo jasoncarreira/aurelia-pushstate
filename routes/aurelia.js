6f0fe40613e10ce63fdd1d72abb70778f03c5f4e
Jason Carreira <jcarreira@gmail.com>
2016-05-22 09:51:43 -0400

    Removing unused route file

--- routes/aurelia.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET anything under our path. */
-router.get('*', function(req, res, next) {
-  response.sendfile('./public/aurelia/index.html');
-});
-
-module.exports = router;
